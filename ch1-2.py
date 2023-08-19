6ba007a14a639752be76d9fe5e924a523a9b290c
AMQ4 <amq4319@gmail.com>
2023-08-19 14:17:47 +0300

    Revert "all ch1 exercises are solved"
    This reverts commit 86534e36209eacb9a11829fdc2f2f23eb4e91e83.

--- ch1-2.py
+++
@@ -1,8 +0,0 @@
-try:
-    n = int(input("Enter the number for the multiplication table:\n> "))
-    max_multiple = int(input("Enter the maximum multiple you want to see:\n> "))
-except ValueError as e:
-    print(e)
-else:
-    for i in range(1, max_multiple+1):
-        print("{0} x {1} = {2}".format(n, i, i*n))
